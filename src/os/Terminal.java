94182ac4d01bff60c76b6b83811ccebb2ef9ddbd
Mohamed Sayed <mohamedsayedaz18@gmail.com>
2020-11-11 01:38:22 +0200

    Create mv function

--- src/os/Terminal.java
+++ src/os/Terminal.java
@@ -3,7 +3,8 @@ package os;
 import java.io.File;
 
 public class Terminal {
-    String current_path = "/home/mohamed/";//This Variable Just Point To The Folder Where iam
+    //This Variable Just Point To The Folder Where iam
+    String current_path = "/home/mohamed/";
     /**
      * mkdir Function
      * this function create a folder in my current directory "current_path"
@@ -34,16 +35,16 @@ public class Terminal {
     /**
      * rmdir Function
      * this function remove a directory
-     * @param name this is then name of the folder that will be created
+     * @param name this is then name of the folder that will be deleted
      * */
     public void rmdir(String name){
         //Contain the path of the folder that will be created
         String dir_path=null;
         //check if the last index of the path is "/" or not
-        if(current_path.charAt(current_path.length()-1) == '/')
+        /*if(current_path.charAt(current_path.length()-1) == '/')
             dir_path = current_path + name;
         else
-            dir_path = current_path + '/' + name;
+            dir_path = current_path + '/' + name;*/
 
         File my_dir =  new File(dir_path);
         //check if it's a valid directory
@@ -61,5 +62,58 @@ public class Terminal {
         }else{
             System.out.println("This folder dosn't exist!");
         }
+    }
+    /**
+     * rm Function
+     * this function remove a file
+     * @param sourcePath this is then filename that will be deleted
+     * */
+    public void rm(String sourcePath){
+        //Contain the path of the folder that will be created
+        String dir_path=null;
+        if(sourcePath.contains(":")){
+            dir_path = sourcePath;
+        }else{
+            dir_path = current_path + '/' + sourcePath;
+        }
+        File my_file =  new File(dir_path);
+        if(my_file.exists()){
+            //check if this path is a directory not a file
+            if(!my_file.isDirectory()){
+                boolean success = my_file.delete();
+                if (success)
+                    System.out.println("The Folder Deleted Successfuly.");
+                else
+                    System.out.println("Something goes wrong!");
+            }else{
+                System.out.println("This Not A Directory!");
+            }
+        }else{
+            System.out.println("This folder dosn't exist!");
+        }
+    }
+    /**
+     * mv Function
+     * this function move file from source path to destination path
+     * @param sourcePath the path of file that will be moved
+     * @param destinationPath the path to move file in it
+     * */
+    public void mv(String sourcePath, String destinationPath){
+        File my_file = new File(sourcePath);
+        File Destination = new File(destinationPath);
+        //check if the file exist
+        if(my_file.exists()){
+            //check if the destination folder
+            if (Destination.exists()){
+                my_file.renameTo(new File(destinationPath));
+                my_file =  new File(sourcePath);
+                my_file.delete();
+            }else{
+                System.out.println("Destination Directory Dosn't exist!");
+            }
+        }else{
+            System.out.println("This File Dosn't Exist!");
+        }
+
     }
 }
