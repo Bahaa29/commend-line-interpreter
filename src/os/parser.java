d7ecb3386dbfc66590f895ee7e9612395ea5b5bd
Bahaa <bahaaeldin390@gmail.com>
2020-11-10 05:43:20 -0800

    basic functions

--- src/os/parser.java
+++ src/os/parser.java
@@ -1,4 +1,30 @@
 package os;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+public class parser
+{
+    String[] args;
+    String cmd=null;
 
-public class parser {
+    public boolean parse(String input)
+    {
+        String command;
+        String[] split_line;
+        split_line =input.split(" ");
+        command=split_line[0];
+
+    }
+    public List<String> getArguments(String command)
+    {
+
+
+    }
+
+    public String getCmd(String input)
+    {
+
+    }
+
+
 }
